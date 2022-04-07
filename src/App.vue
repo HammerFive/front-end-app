cd7fcae3a372454e146386eec52c4aea07081c1d
Hammerfive <hammerfive@five.local>
2022-04-07 16:18:01 +0800

    🎉 初始: 初始化vue3+vitejs+tailwind 前端脚手架项目
    undefined

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <script setup>
-import { ref } from 'vue'
+  import { ref } from 'vue'
 
-let counter = ref(0)
+  const counter = ref(0)
 
-setInterval(() => {
-  counter.value++
-}, 1000)
+  setInterval(() => {
+    counter.value++
+  }, 1000)
 </script>
 
 <template>
   <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
+    <header
+      v-if="$route.meta.title"
+      class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1
-          @click="counter = 0"
           class="text-3xl font-bold leading-tight text-gray-900"
-        >
+          @click="counter = 0">
           {{ $route.meta.title }} / {{ counter }}
         </h1>
       </div>
